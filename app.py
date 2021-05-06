53d9369936c9d719d95612c1efdd5e9a90c4a6cf
aliclarke206 <alison.clarke29@mail.dcu.ie>
2021-05-06 12:27:09 +0000

    added footer template

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def edit_hike(hike_id):
 
 @app.route("/delete_hike/<hike_id>")
 def delete_hike(hike_id):
-    mongo.db.hikes.remove({"_id": ObjectId(hike_id)})
+    mongo.db.hikes.delete_one({"_id": ObjectId(hike_id)})
     flash("Hike Successfully Deleted")
     return redirect(url_for("get_hikes"))
 
